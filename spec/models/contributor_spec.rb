27e9cfef4aff22c327bb5e9a9ef8795a63e76e2d
Samuel Paget <sjapaget@gmail.com>
2022-09-29 19:52:02 +0200

    Add validity tests for presence on contributor model

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Contributor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # setup - create a user for the contributor to belong to
+  test_user = User.new(first_name: "chuck", last_name: "the-cat", email: "chuck-the-cat@cats.fr", password: "123456")
+
+  describe "#name" do
+    it 'validates presence' do
+      contributor = Contributor.new
+      contributor.user = test_user
+      contributor.validate
+      expect(contributor.errors[:name]).to include("can't be blank")
+
+      contributor.name = "Chuck The-cat"
+      contributor.validate
+      expect(contributor.errors[:name]).to_not include("can't be blank")
+    end
+  end
+
+  describe "#user" do
+    it 'validates presence' do
+      contributor = Contributor.new
+      contributor.name = "Chuck The-cat"
+      contributor.validate
+      expect(contributor.errors[:user]).to include("can't be blank")
+
+      contributor.user = test_user
+      contributor.validate
+      expect(contributor.errors[:user]).to_not include("can't be blank")
+    end
+  end
 end
