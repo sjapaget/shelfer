ad39cc39225ec0c3463f8e683fae51e3719c631b
Samuel Paget <sjapaget@gmail.com>
2022-10-02 10:50:31 +0200

    Remove minitest as we're using RSpec

--- test/models/user_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class UserTest < ActiveSupport::TestCase
-  test "full_name returns the capitalized first name and last name" do
-    user = User.new(first_name: "chuck", last_name: "the-cat")
-
-    assert_equal "Chuck The-cat", user.full_name
-  end
-end
