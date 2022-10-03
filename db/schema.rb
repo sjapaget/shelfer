f1a87ed23977cd749d58e05ef24836a744b7a39c
clara basuyau <clarabasuyau@gmail.com>
2022-10-03 14:09:16 +0200

    create book controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_081444) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_093930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.string "title"
     t.integer "number_of_pages"
     t.text "blurb"
-
   end
 
   create_table "contributors", force: :cascade do |t|
