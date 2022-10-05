c4892fe65b9c06be20ff9089bcbb8f4e59398986
clara basuyau <clarabasuyau@gmail.com>
2022-10-05 11:23:34 +0200

    added validations to shelf model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :contributors, dependent: :destroy
+  has_many :shelves, dependent: :destroy
 
   validates :first_name, :last_name, presence: true
   validates :email, uniqueness: true
