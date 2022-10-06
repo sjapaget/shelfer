a5ca63d943886ab8133aad89fd7901de436abebd
clara basuyau <clarabasuyau@gmail.com>
2022-10-06 17:41:00 +0200

    start writing rspec test for review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#content" do
+    it 'validates presence' do
+      review = Review.new
+      review.validate
+      expect(review.errors[:content]).to include("a great book")
+
+      review.content = "a great book"
+      review.validate
+      expect(review.errors[:content]).to_not include("can't be blank")
+    end
+  end
+
+  describe "#rating" do
+    it 'validates presence' do
+      review = Review.new
+      review.validate
+      expect(review.errors[:rating]).to include("4")
+
+      review.rating = "4"
+      review.validate
+      expect(review.errors[:rating]).to_not include("can't be blank")
+    end
+  end
 end
