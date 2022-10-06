f74c0d8d4232e87b365254a882af27a9153b5b31
Samuel Paget <sjapaget@gmail.com>
2022-10-06 17:21:23 +0200

    Add validations for Contribution model

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -5,6 +5,20 @@ RSpec.describe Contribution, type: :model do
   test_contributor = Contributor.create!(user: test_user, name: "Chuck The Cat")
   test_book = Book.create!(title: "The life of a Cat", number_of_pages: 55, blurb: "Chuck gives insights on his life as a cat")
 
+  describe "#unique_combination_of_book_contributor_and_role" do
+    it "must validate on uniqueness of the contributor, book, role combination" do
+      Contribution.create!(contributor: test_contributor, book: test_book, role: Contribution::ROLES[0])
+
+      contribution = Contribution.new(contributor: test_contributor, book: test_book, role: Contribution::ROLES[0])
+      contribution.validate
+      expect(contribution.errors[:contributor]).to include("this contribution already exists")
+
+      contribution.role = Contribution::ROLES[1]
+      contribution.validate
+      expect(contribution.errors[:contributor]).to_not include("this contribution already exists")
+    end
+  end
+
   describe "#book" do
     it "must be present" do
       contribution = Contribution.new
@@ -35,18 +49,6 @@ RSpec.describe Contribution, type: :model do
       contribution.contributor = test_contributor
       contribution.validate
       expect(contribution.errors[:contributor]).to_not include("can't be blank")
-    end
-
-    it "must be unique for the given book and role" do
-      Contribution.create!(contributor: test_contributor, book: test_book, role: Contribution::ROLES[0])
-
-      contribution = Contribution.new(contributor: test_contributor, book: test_book, role: Contribution::ROLES[0])
-      contribution.validate
-      expect(contribution.errors[:contributor]).to include("can't be assigned twice")
-
-      contribution.role = Contribution::ROLES[1]
-      contribution.validate
-      expect(contribution.errors[:contributor]).to_not include("can't be assigned twice")
     end
 
     it "if destroyed, the contribution must also be destroyed" do
