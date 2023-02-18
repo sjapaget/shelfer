b399555ae9b9b283101d5cb9f03d2e50f10bb430
Samuel Paget <sjapaget@gmail.com>
2023-02-18 23:50:41 +0100

    Remove #to_builder

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -9,10 +9,4 @@ class Contributor < ApplicationRecord
     contributions = book.contributions.where(contributor: self)
     contributions.map(&:role)
   end
-
-  def to_builder
-    Jbuilder.new do |contributor|
-      contributor.name name
-    end
-  end
 end
