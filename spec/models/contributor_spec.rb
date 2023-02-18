806ac6b031883b55b2281ff80027d694e921038f
Samuel Paget <sjapaget@gmail.com>
2023-02-18 22:52:41 +0100

    Add test Contributor#role

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Contributor, type: :model do
-  # Put some tests here
+  describe "#role" do
+    let(:book) { build(:book) }
+    let(:contributor) { build(:contributor) }
+    let(:contribution) { create(:contribution, book: book, contributor: contributor) }
+    it "returns the role of the contributor for a given book" do
+      result = contributor.role(book)
+      expect(result).to eq('author')
+    end
+  end
 end
