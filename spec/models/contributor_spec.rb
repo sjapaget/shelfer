3a396272f2d8ec8e71b2e0448575bcdfcf221dc5
Samuel Paget <sjapaget@gmail.com>
2023-02-18 23:15:19 +0100

    Add Contributor#role(book)

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe Contributor, type: :model do
   describe "#role" do
     let(:book) { build(:book) }
     let(:contributor) { build(:contributor) }
-    let(:contribution) { create(:contribution, book: book, contributor: contributor) }
+    let!(:contribution) { create(:contribution, book: book, contributor: contributor) }
     it "returns the role of the contributor for a given book" do
       result = contributor.role(book)
-      expect(result).to eq('author')
+      expect(result).to contain_exactly('author')
+    end
+
+    context "when a contributor has multiple roles for the same book" do
+      let!(:editor_contribution) { create(:contribution, :editor, book: book, contributor: contributor) }
+      it "returns all the roles" do
+        result = contributor.role(book)
+        expect(result).to contain_exactly('author', 'editor')
+      end
     end
   end
 end
