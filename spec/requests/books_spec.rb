58bcabd8503bdae6c5a8b5c1f95d06e98ee64af1
sam <sjapaget@gmail.com>
2023-02-24 21:09:21 +0100

    Add simple request spec

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:user) { create(:user) }
+    let!(:book) { create(:book) }
+    before do
+      sign_in user
+    end
+
+    it "displays all the books" do
+      get api_v1_books_path
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(book.title)
+    end
   end
 end
