b89eb0a5cac15e9b1a83b148c85f53b52a82fa7c
sam <sjapaget@gmail.com>
2023-02-25 19:17:02 +0100

    Add #create for books api v1

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,4 +8,18 @@ class Api::V1::BooksController < ApiController
 
     authorize @book
   end
+
+  def create
+    @book = Book.new(book_params)
+
+    @book.save
+    redirect_to api_v1_book_path(@book)
+    authorize @book
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :number_of_pages, :blurb)
+  end
 end
--- app/policies/book_policy.rb
+++ app/policies/book_policy.rb
@@ -3,6 +3,10 @@ class BookPolicy < ApplicationPolicy
     true
   end
 
+  def create?
+    true
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
