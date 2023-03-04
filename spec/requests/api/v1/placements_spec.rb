f90d0210cb04d083762a5a68f58451a7e43c9044
sam <sjapaget@gmail.com>
2023-03-04 20:17:08 +0100

    Add request spec for PlacementsController#create

--- spec/requests/api/v1/placements_spec.rb
+++ spec/requests/api/v1/placements_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Placements", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /create" do
+    let(:valid_attributes) { {} }
+    let(:invalid_attributes) { {} }
+
+    context 'with valid parameters' do
+      it 'creates a new placemet' do
+        expect {
+          post api_v1_placements_path, params: { placement: valid_attributes }
+        }.to change(Placement, :count).by(1)
+      end
+
+      it "redirects to the shelf index of the created placement" do
+        post api_v1_placements_path, params: { placement: valid_attributes }
+        expect(response).to redirect_to(api_v1_shelves_path(Placement.last.shelf))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it "doesn't create a new placement" do
+        expect {
+          post api_v1_placements_path, params: { placement: invalid_attributes }
+        }.to change(Placement, :count).by(0)
+      end
+
+      it 'responds with status 422 - unprocessable entity' do
+        post api_v1_placements_path, params: { placement: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
