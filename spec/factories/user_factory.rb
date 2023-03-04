e5ca3f15a9624f383261cc78bd238bb7471666a9
sam <sjapaget@gmail.com>
2023-03-04 20:53:54 +0100

    Add sequences to user, shelf & placement factories

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.fr"
+  end
+
+
   factory :user do
     first_name { "test" }
     last_name  { "profile" }
-    email { 'rspec@test.fr' }
+    email { generate :email }
     password { 'password' }
   end
 
